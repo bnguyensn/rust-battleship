b2daad97a55ac1866addae688d8466356fd238e3
Binh Nguyen <b.nguyen.sn@gmail.com>
2024-11-09 00:12:25 +0000

    add a function to ask for shooting target

--- src/board.rs
+++ src/board.rs
@@ -25,6 +25,7 @@ const SHIP_PLACEMENT_INVALID_ORIENTATION_MSG: &str =
     "Invalid orientation. Please enter either 'H' for horizontal or 'V' for vertical.";
 const SHIP_PLACEMENT_OVERLAP_MSG: &str =
     "Ship placement overlaps with another ship. Please try again.";
+const SHOOTING_INVALID_INPUT_MSG: &str = "Invalid input. Please enter coordinates in the form 'x y' where x and y are integers, and are separated by a single space.";
 const FAILED_TO_READ_INPUT_MSG: &str = "Failed to read input.";
 
 #[cfg(debug_assertions)]
@@ -75,14 +76,12 @@ impl Board {
     pub fn ask_for_ship_placement(&mut self) -> (usize, usize, Orientation) {
         println!("Enter the coordinate and orientation for your ship e.g. 3 4 H");
         'ship_placement: loop {
-            let mut placement_input = String::new();
-            match io::stdin().read_line(&mut placement_input) {
+            let mut input = String::new();
+            match io::stdin().read_line(&mut input) {
                 Ok(_) => {
-                    let parts: Vec<&str> = placement_input.trim().split_whitespace().collect();
-
+                    let parts: Vec<&str> = input.trim().split_whitespace().collect();
                     #[cfg(debug_assertions)]
                     debug_parts(&parts);
-
                     if parts.len() != 3 {
                         println!("{}", SHIP_PLACEMENT_INVALID_INPUT_MSG);
                         continue;
@@ -140,7 +139,7 @@ impl Board {
                         }
                     }
                 }
-                Err(_) => {
+                _ => {
                     println!("{}", FAILED_TO_READ_INPUT_MSG);
                 }
             }
@@ -153,25 +152,38 @@ impl Board {
     /// Returns the coordinates as a u32 tuple.
     /// Loops until the player enters valid coordinates.
     fn ask_for_shoot_target(&self) -> (usize, usize) {
+        println!("Enter a coordinate to shoot at e.g. 3 4");
         loop {
-            println!("Enter a coordinate to shoot at e.g. 3 4");
-
             let mut input = String::new();
-            io::stdin()
-                .read_line(&mut input)
-                .expect("Failed to read line");
 
-            let parts: Vec<&str> = input.trim().split_whitespace().collect();
-            if parts.len() == 2 {
-                if let (Ok(x), Ok(y)) = (parts[0].parse::<usize>(), parts[1].parse::<usize>()) {
-                    if x < self.grid_size && y < self.grid_size {
-                        return (x, y);
-                    } else {
-                        println!("Coordinates out of bounds. Please enter values between 0 and {} for x and 0 and {} for y.", self.grid_size - 1, self.grid_size - 1);
+            match io::stdin().read_line(&mut input) {
+                Ok(_) => {
+                    let parts: Vec<&str> = input.trim().split_whitespace().collect();
+                    #[cfg(debug_assertions)]
+                    debug_parts(&parts);
+                    if parts.len() != 2 {
+                        println!("{SHOOTING_INVALID_INPUT_MSG}");
+                        continue;
+                    }
+                    let (x, y) = (parts[0].parse::<usize>(), parts[1].parse::<usize>());
+
+                    match (x, y) {
+                        (Ok(x), Ok(y)) => {
+                            if x >= self.grid_size || y >= self.grid_size {
+                                println!("Coordinate out of bounds. Please enter values between 0 and {} for x and 0 and {} for y.", self.grid_size - 1, self.grid_size - 1);
+                                continue;
+                            }
+                            return (x, y);
+                        }
+                        _ => {
+                            println!("{FAILED_TO_READ_INPUT_MSG}");
+                        }
                     }
                 }
+                _ => {
+                    println!("{FAILED_TO_READ_INPUT_MSG}");
+                }
             }
-            println!("Invalid input. Please enter coordinates in the form 'x y' where x and y are integers, and are separated by a single space.");
         }
     }
 
